ced9eb60fa1f3e112b03d77769e08077163ccca8
ratul6895 <159277416+ratul6895@users.noreply.github.com>
2026-08-25 10:50:51 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,25 +1,31 @@
 import os
 
 class Config(object):
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	APP_ID = int(os.environ.get("APP_ID"))
-	API_HASH = os.environ.get("API_HASH")
-	DATABASE_URL = os.environ.get("DATABASE_URL")
-	SUDO_USERS = list(set(int(x) for x in ''.split()))
-	SUDO_USERS.append(853393439)
-	SUDO_USERS = list(set(SUDO_USERS))
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("APP_ID", os.environ.get("API_ID", 0)))
+    API_HASH = os.environ.get("API_HASH", "")
+    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "")
+    OWNER_ID = int(os.environ.get("OWNER_ID", 0))
+    
+    # SUDO USERS SUPPORT
+    sudo_env = os.environ.get("SUDO_USERS", "")
+    SUDO_USERS = list(set(int(x) for x in sudo_env.split())) if sudo_env else []
+    if OWNER_ID:
+        SUDO_USERS.append(OWNER_ID)
+    SUDO_USERS.append(853393439)  # ডিফল্ট অ্যাডমিন আইডি
+    SUDO_USERS = list(set(SUDO_USERS))
 
 class Messages():
-      HELP_MSG = [
+    HELP_MSG = [
         ".",
 
-        "[🔔](https://i.imgur.com/SmqQApH.jpg) **FORCE SUBSCRIBE :**\n\nForce Group Members To Join A Specific Channel Before Sending Messages in The Group.\nI Will Mute Members if They Not Joined Your Channel And Tell Them To Join The Channel And Unmute Themself By Pressing A Button.",
+        "[🔔](https://i.imgur.com/SmqQApH.jpg) **FORCE SUBSCRIBE (বাধ্যতামূলক সাবস্ক্রিপশন) :**\n\nগ্রুপে কোনো বার্তা বা মেসেজ পাঠানোর পূর্বে সদস্যদের একটি নির্দিষ্ট চ্যানেলে যুক্ত হতে বাধ্য করে।\nসদস্যরা চ্যানেলে যুক্ত না হলে তাদের পাঠানো বার্তা ডিলিট করা হবে এবং চ্যানেলে যুক্ত হওয়ার বার্তা ও বাটন দেখানো হবে।",
         
-        "[⚙](https://i.imgur.com/ItAdRVF.jpg) **SETUP :**\n\nFirst Of All Add Me In The Group As Admin With Ban Users Permission And In The Channel As Admin.\n● Note: Only Creator Of The Group Can Setup Me.",
+        "[⚙](https://i.imgur.com/ItAdRVF.jpg) **সেটআপ নির্দেশিকা :**\n\nপ্রথমেই আমাকে আপনার গ্রুপে **Admin** হিসেবে যুক্ত করুন (Delete Messages পারমিশনসহ) এবং সংশ্লিষ্ট চ্যানেলেও Admin বানান।\n● **নোট:** শুধুমাত্র গ্রুপের মালিক (Creator) বা অ্যাডমিনরা এটি সেটআপ করতে পারবেন।",
         
-        "[⚙](https://i.imgur.com/LnOEiTK.jpg) **COMMMANDS :**\n\n/ForceSubscribe - To Get The Current Settings.\n/ForceSubscribe no/off/disable - To Turn Of ForceSubscribe.\n/ForceSubscribe {Channel Username} - To Turn On And Setup The Channel.\n/ForceSubscribe clear - To Unmute All Members Who Muted By Me.\n\n● Note: /FSub Is An Alias Of /ForceSubscribe",
+        "[⚙](https://i.imgur.com/LnOEiTK.jpg) **কমান্ডসমূহ :**\n\n/ForceSubscribe - বর্তমান সেটিংস দেখতে।\n/ForceSubscribe no/off/disable - ForceSubscribe বন্ধ করতে।\n/ForceSubscribe {Channel Username/Link} - নতুন চ্যানেল সেটআপ করতে।\n\n● **নোট:** `/FSub` বা `/fsub` হলো `/ForceSubscribe` কমান্ডের সংক্ষিপ্ত রূপ।",
         
-        "[👨‍💻](https://telegra.ph/file/f2b08ba94ebd139d9da96.jpg) **DEVELOPED BY @AmineSoukara**"
-      ]
+        "[👨‍💻](https://telegra.ph/file/f2b08ba94ebd139d9da96.jpg) **ডেভেলপার ও সহায়তা**\n\nবট সংক্রান্ত যেকোনো সমস্যায় ওনারের সাথে যোগাযোগ করুন।"
+    ]
 
-      START_MSG = "**Hey! [👋](https://i.imgur.com/SmqQApH.jpg) [{}](tg://user?id={})**\n\n● I Can Force Members To Join A Specific Channel Before Writing Messages In The Group.\n● Learn More At 👉 /help"
+    START_MSG = "**হ্যালো! [👋](https://i.imgur.com/SmqQApH.jpg) [{}](tg://user?id={})**\n\n● আমি আপনার গ্রুপের সদস্যদের নির্দিষ্ট চ্যানেলে যুক্ত হতে বাধ্য করতে পারি।\n● বিস্তারিত জানতে ক্লিক করুন 👉 /help"
